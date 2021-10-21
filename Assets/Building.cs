938a8d90434b3b5206e8fdeab5e68812004c0225
ochggm <jjabjam0002@naver.com>
2021-10-21 12:32:57 +0900

    오창현 빌딩 이미지 추가

--- Assets/Building.cs
+++ Assets/Building.cs
@@ -2,28 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 public class Building : MonoBehaviour
 {
-    
+    object[] sprites; 
     Button Clicker;
     public int fullhp;
     int DeathMoney=5;
     public int hp;
     public Image hpbar;
     int BreakLevel;
-    const int BreakCount = 5;
+    const int BreakCount = 6;
+    Image SP;
     public float[] pos = new float[BreakCount];
 
     private void Awake()
     {
-        
-        print("AWAkewasstartd");
+        sprites = Resources.LoadAll("Building");
+         SP = GetComponent<Image>();
+         SP.sprite = (Sprite)sprites[5];
+        print(sprites);
         for(int i=0;i<BreakCount;i++)
         {
             pos[i] = (1 / (float)(BreakCount-1))* i;
             print(i);
         }
-
         Clicker = GetComponent<Button>();
         Clicker.onClick.RemoveAllListeners();
         Clicker.onClick.AddListener(() => {
@@ -35,8 +38,8 @@ public class Building : MonoBehaviour
     public void Clicked(int damage)
     {
         //´åÆ®¿ø
+        transform.DOShakePosition(1f,10);
         hp -= damage;
-        
         if(hp<=0)
         {
             GameManager.Instance.money += DeathMoney;
@@ -47,17 +50,23 @@ public class Building : MonoBehaviour
         
         hpbar.fillAmount = hp / (float)fullhp;
         int BreakLevel = 0;
-        for(int i=0; i<BreakCount-2; i++)
+        for(int i=0; i<BreakCount-1; i++)
         {
             if (pos[i] <= hpbar.fillAmount && hpbar.fillAmount < pos[i + 1])
             {
-                BreakLevel = i+1;
+                BreakLevel = i;
                 break;
             }
                 
         }
-
-        if (!(this.BreakLevel == BreakLevel)) ;
+        //if (!(this.BreakLevel == BreakLevel))
+        {
+            this.BreakLevel = BreakLevel;
+            print(BreakLevel);
+            
+                
+            SP.sprite = (Sprite)sprites[BreakLevel];
+        }
            
 
         
